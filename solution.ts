7ff3b4b973e9063c7a4b80064b4765d1cca18366
shovan-642 <shovan642@gmail.com>
2025-11-18 15:14:40 +0600

    problem 1 solved

--- solution.ts
+++ solution.ts
@@ -1,31 +1,13 @@
-/*
-- This solution.ts file is an example; replace it with your own code.
-- Use the same function names and parameter names as in the problem statement.
-- Write only the code inside the function bodies and return the result.
-- Do not use any console.log statements or comments.
-*/
-
-const getSwallowVelocity = (type: 'african' | 'european'): string => {
-  if (type === 'african') {
-    return 'Roughly 11 meters per second.';
+function formatValue (value: string | number | boolean) : string | number | boolean {
+  if(typeof value === "string"){
+    return value.toUpperCase();
   }
-  return "I... I don't know that!";
-};
-
-const isCatLiquid = (isAsleep: boolean, containerShape: string): boolean => {
-  return isAsleep && containerShape !== 'none';
-};
-
-class Wizard {
-  name: string;
-  favoriteSpell: string;
-
-  constructor(name: string, favoriteSpell: string) {
-    this.name = name;
-    this.favoriteSpell = favoriteSpell;
+  if(typeof value === "number"){
+    return value*10
   }
-
-  castSpell(): string {
-    return `${this.name} casts ${this.favoriteSpell}!`;
-  }
+  else return !value
 }
+
+console.log(formatValue('hello'));
+console.log(formatValue(5));
+console.log(formatValue(true));
