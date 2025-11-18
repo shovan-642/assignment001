57bc22fc3fcb2040624630d40e66d89b884994fd
shovan-642 <shovan642@gmail.com>
2025-11-18 16:58:12 +0600

    problem 6 solved

--- solution.ts
+++ solution.ts
@@ -87,5 +87,28 @@ function filterActiveUsers(users: User[]): User[] {
 
 // console.log(filterActiveUsers(users));
 
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+  const status = book.isAvailable ? "Yes" : "No";
+
+  console.log(`Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${status}`)
+}
 
 
+
+
+
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook);
